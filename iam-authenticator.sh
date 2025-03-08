d58f8bd9617da8bcbd0c698a159ac09b1859d9d6
Chimezie Osamor <165746325+osereme-project@users.noreply.github.com>
2025-03-08 12:37:59 -0500

    Update iam-authenticator.sh
    Correcting the aim-authenticator.sh

--- iam-authenticator.sh
+++ iam-authenticator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator curl -o aws-iam-authenticator
 curl -o aws-iam-authenticator.sha256 https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator.sha256
 openssl sha1 -sha256 aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
